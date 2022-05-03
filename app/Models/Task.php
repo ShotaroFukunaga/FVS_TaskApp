9c034e1f0a632149b72f2830eb84ed29f607a69c
ShotaroFukunaga <accelerator.engin@gmail.com>
2022-05-03 23:56:33 +0900

    [create]Factory_to_Task #3
    [create]Seeder_to_Tasks_and_Users

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -17,4 +17,9 @@ class Task extends Model
         'content',
         'deadline'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
@@ -17,7 +18,10 @@ class TaskFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_email' => User::factory(),
+            'title' => $this->faker->realtext(10),
+            'content' => $this->faker->realText(100),
+            'status' => '1',
         ];
     }
 }
